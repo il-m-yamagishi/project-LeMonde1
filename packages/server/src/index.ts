e78da1815790b02e4be56f33ee1d0c6f6560e78c
m-yamagishi <m-yamagishi@infiniteloop.co.jp>
2021-11-26 10:19:53 +0900

    Add websocket

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,10 +1,36 @@
-import express from 'express';
+import express, { Request } from 'express';
 import helmet from 'helmet';
+import expressWs from 'express-ws';
+import { WebSocket } from 'ws';
 
 const app = express();
 
-app.use(helmet());
+expressWs(app);
 
+app.use(helmet());
 app.use(express.static(`${__dirname}/../static`));
+(app as any).ws('/ws', handleWebSocket);
 
 app.listen(8080);
+
+let totalClicked = 0;
+
+function handleWebSocket(ws: WebSocket, req: Request) {
+    console.log('new user');
+    ws.send(JSON.stringify({action: 'init', totalClicked}))
+    ws.on('message', function (data) {
+        console.log('on message', data);
+        if (typeof data === 'string') {
+            const payload = JSON.parse(data);
+            switch (payload.action) {
+                case 'click':
+                    totalClicked++;
+                    console.log('totalClicked = ', totalClicked);
+                    break;
+                default:
+                    console.error('Unknown action');
+                    break;
+            }
+        }
+    });
+}
--- packages/server/static/client.js
+++ packages/server/static/client.js
@@ -1,13 +1,48 @@
+async function main() {
+    let clickCount = 0;
+    const countDom = document.querySelector('#count');
+    const titleDom = document.querySelector('title');
+    const ogTitleDom = document.querySelector('#og_title');
 
-let clickCount = 0;
-const countDom = document.querySelector('#count');
-countDom.innerHTML = clickCount.toLocaleString();
-const catDom = document.querySelector('#cat');
-catDom.addEventListener('click', (e) => {
-    e.preventDefault();
-    clickCount++;
-    countDom.innerHTML = clickCount.toLocaleString();
-});
-catDom.addEventListener('dragstart', (e) => {
-    e.preventDefault();
-});
+    updateCount();
+
+    function updateCount() {
+        countDom.innerText = clickCount.toLocaleString();
+        titleDom.innerText = `${clickCount.toLocaleString()}回クリックされた猫`;
+        ogTitleDom.innerText = `${clickCount.toLocaleString()}回クリックされた猫`;
+    }
+
+    function sendClick() {
+        if (ws) {
+            ws.send(JSON.stringify({action: 'click'}));
+        }
+    }
+
+    function onClick(e) {
+        e.preventDefault();
+        clickCount++;
+        sendClick();
+        updateCount();
+    };
+
+    const catDom = document.querySelector('#cat');
+    catDom.addEventListener('click', onClick);
+    catDom.addEventListener('dragstart', (e) => {
+        e.preventDefault();
+    });
+
+    const host = location.origin.replace(/^http/, 'ws') + '/ws';
+    const ws = new WebSocket(host);
+    ws.onmessage = (ev) => {
+        console.log('receive', ev);
+        const payload = JSON.parse(ev.data);
+        switch (payload.action) {
+            case 'init':
+                clickCount = payload.totalClicked;
+                updateCount();
+                break;
+        }
+    };
+}
+
+main();
